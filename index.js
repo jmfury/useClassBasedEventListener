50836ce0e3149517345f94475da74b1c50b5b631
Jimmy <jimmy@hashicorp.com>
2019-06-30 13:14:24 -0500

    Change event param naming

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export default function useClassBasedEventListener(
   elementClass,
-  event,
+  eventParam,
   handler,
   passive = true
 ) {
@@ -12,13 +12,13 @@ export default function useClassBasedEventListener(
 
     // Add the event listener along with the function to run when event is emitted
     for (let i = 0; i < classnameArray.length; i++) {
-      elementArray[i].addEventListener(event, handler, passive)
+      elementArray[i].addEventListener(eventParam, handler, passive)
     }
 
     // this will clean up the eventListener when the component is unmounted
     return function cleanup() {
       for (let i = 0; i < elementArray.length; i++) {
-        elementArray[i].removeEventListener(event, handler)
+        elementArray[i].removeEventListener(eventParam, handler)
       }
     }
   }, [])

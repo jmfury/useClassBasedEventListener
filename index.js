c4f50e139036b3bcb68d270cfd6155f9808f2833
Jimmy Merritello <jimmy@hashicorp.com>
2019-06-30 21:00:33 -0500

    Update comments

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ export default function useClassBasedEventListener(
     // Get all the elements to listen on
     let elementArray = document.getElementsByClassName(elementClass)
 
-    // Add the event listener along with the function to run when event is emitted
+    // Add the event listener to all elements in the array
     for (let i = 0; i < classnameArray.length; i++) {
       elementArray[i].addEventListener(eventParam, handler, passive)
     }
 
-    // this will clean up the eventListener when the component is unmounted
+    // this will clean up the event listener when the component is unmounted
     return function cleanup() {
       for (let i = 0; i < elementArray.length; i++) {
         elementArray[i].removeEventListener(eventParam, handler)
